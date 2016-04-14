4c71f38bdcccbcd282633934b63ac8c52f010d3c
谢建超 <jianchaoxie@anjuke.com>
2016-04-14 14:20:18 +0800

    da

---
+++ Test/Test.php
@@ -0,0 +1,11 @@
+<?php
+	namespace Test;
+	class Test {
+		public function __construct() {
+
+		}	
+		public function sayhello() {
+			echo "hello";
+		}
+	}
+?>
--- test/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-	namespace test;
-	class test {
-		public function sayhello() {
-			echo "hello";
-		}
-	}
-?>
